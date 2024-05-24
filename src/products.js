b9b39166e863e3181de6f3ed8325fbddd4ddd450
Juan <juanpfreire18@gmail.com>
2024-05-24 10:54:57 -0300

    [feat] creating array of store product objects

---
+++ src/products.js
@@ -0,0 +1,31 @@
+import notebook from './assets/notebook.png'
+import Iphone from './assets/iphone.png'
+import play5 from './assets/play5.png'
+import gucci from './assets/gucci.png'
+
+export const PRODUCTS = [
+    {
+        id: 1,
+        productName: "Notebook",
+        price: 1800.00,
+        productImage: notebook,
+    },
+    {
+        id: 2,
+        productName: "IPhone",
+        price: 1450.00,
+        productImage: Iphone,
+    },
+    {
+        id: 3,
+        productName: "Playstation 5",
+        price: 4500.00,
+        productImage: play5,
+    },
+    {
+        id: 4,
+        productName: "Gucci",
+        price: 499.99,
+        productImage: gucci,
+    }
+]
